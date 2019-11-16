c06873f4f2b72ced12cd37730694743a4ec6f376
Saulo Queiroz <sauloqueiroz@utfpr.edu.br>
2019-11-16 16:07:24 -0300

    Addedd ieeeaccess program

--- examples/experiments.sh
+++ examples/experiments.sh
@@ -16,7 +16,7 @@ echo "ATTENTION: This scripts works only for BPSK and even numbers N<=68"
 #-- ieeeaccess produces 20000 runtime samples. I verified this is enough
 #   to enable the convergence of akaroa-2 with 95% conf. and rel. error < 5%
 
-LARGESTN=68
+LARGESTN=44
 for ALGORITHM in {1..2..1} 
 do
 #    for i in {2..$LARGESTN..2} 
@@ -51,15 +51,18 @@ sudo killall ieeeaccess
 # computing m requires calculation of binomial coefficients, which is hard shell.
 # Thus I compute them offline for different Ns and load them in an array here.
 # WE ASSUME LARGESTN = 68 AND N is even
-m=([1]=2 [2]=4 [3]=7 [4]=10 [5]=12 [6]=15 [7]=18 [8]=21 [9]=24 [10]=27 [11]=30 [12]=33 [13]=36 [14]=39 [15]=42 [16]=45 [17]=48 [18]=51 [19]=54 [20]=57 [21]=59 [22]=62 [23]=65 [24]=68 [25]=71 [26]=74 [27]=77 [28]=80 [29]=83 [30]=86 [31]=89 [32]=92 [33]=95[34]=98)
+m=([1]=2 [2]=4 [3]=7 [4]=10 [5]=12 [6]=15 [7]=18 [8]=21 [9]=24 [10]=27 [11]=30 [12]=33 [13]=36 [14]=39 [15]=42 [16]=45 [17]=48 [18]=51 [19]=54 [20]=57 [21]=59 [22]=62 [23]=65 [24]=68 [25]=71 [26]=74 [27]=77 [28]=80 [29]=83 [30]=86 [31]=89 [32]=92 [33]=95 [34]=98)
 
 
-rm runtime-adapted-vs-combinadic-data.txt
-rm scthroughput-adapted-vs-combinadic-data.txt
-rm latex-table.txt
-rm se-gain.txt
-
+sudo rm runtime-adapted-vs-combinadic-data.txt
+sudo rm scthroughput-adapted-vs-combinadic-data.txt
+sudo rm latex-table.txt
+sudo rm se-gain.txt
+sudo rm sc.eps
+sudo rm runtime.eps
+prec=0.0000000001
 # generate latex table and file for gnuplot
+echo "0 0 0 0 0" >> scthroughput-adapted-vs-combinadic-data.txt
 for ((i=2; i<=$LARGESTN; i=i+2 ))
 do
     #awk 'NR==2 {print $3}' FILE.txt means: output the content of column 3 of line 2 of file FILE.txt
@@ -75,21 +78,36 @@ do
     nadapted=`awk 'NR==2 {print $6}' "resultado-2-$i"`
     transienteadapted=`awk 'NR==2 {print $7}' "resultado-2-$i"`
     # file to plot runtimes with gnuplot i.e. NON rounded values
-    echo "$i $tempoadapted $deltaadapted $tempocomb $deltacomb" >> runtime-adapted-vs-combinadic-data.txt
-
-   # so reportamos ate n=LARGESTN 
-   if [ $i -le $LARGESTN ]
-   then   
     j=$((i / 2)); #indice do array de bits
     bits=`echo ${m[$j]}` #bits conseguidos para essa configuracao IM
-    ## calculating SC throughput to gnuplot i.e. NON rounded values
-    #tempocombsegs=`bc <<< "scale = 6; $tempocomb/1000000"` 
-    #tempoadaptedsegs=`bc <<< "scale = 6; $tempoadapted/1000000"` 
+    echo "$i $tempoadapted $deltaadapted $tempocomb $deltacomb $bits" >> runtime-adapted-vs-combinadic-data.txt
 
-    #scadapted=`bc <<< "scale = 3; $bits / $tempoadaptedsegs"`; # Spectro COmputational throughput
-    #sccomb=`bc <<< "scale = 3; $bits / $tempocombsegs"`; # Spectro COmputational throughput
-    ## file to plot spectro computational throughput with gnuplot.
-    #echo "$i $scadapted $deltaadapted $sccomb $deltacomb" >> scthroughput-adapted-vs-combinadic-data.txt
+   # so reportamos ate n=LARGESTN 
+   if [ $i -le $LARGESTN ];
+   then   
+    #j=$((i / 2)); #indice do array de bits
+    #bits=`echo ${m[$j]}` #bits conseguidos para essa configuracao IM
+    ## calculating SC throughput to gnuplot i.e. NON rounded values
+    
+    tempocombsegs=`bc <<< "scale = 10; $tempocomb/1000000"` 
+    tempoadaptedsegs=`bc <<< "scale = 10; $tempoadapted/1000000"` 
+    if (( $(echo "$tempocombsegs < $prec" |bc -l) ))
+    then
+       tempocombsegs=$prec
+    fi
+    if (( $(echo "$tempoadaptedsegs< $prec" |bc -l) ))
+    then
+       tempoadaptedsegs=$prec
+    fi
+    echo "Tempo em segundos $tempocombsegs $tempoadaptedsegs";
+    echo "Bits=$bits"
+    sccomb=`bc <<< "scale = 10; $bits/$tempocombsegs"`; # Spectro COmputational throughput
+    sccomb=`bc <<< "scale = 10; $sccomb/1000000"`; # bps to mbps
+    scadapted=`bc <<< "scale = 10; $bits/$tempoadaptedsegs"`; # Spectro COmputational throughput
+    scadapted=`bc <<< "scale = 10; $scadapted/1000000"`; # bps to mbps
+    echo "SC Mbps = $scomb $scadapted"
+    # file to plot spectro computational throughput with gnuplot.
+    echo "$i $scadapted $deltaadapted $sccomb $deltacomb" >> scthroughput-adapted-vs-combinadic-data.txt
 
     # rounding digits of time and sc values for paper
     tempoadapted=`echo $tempoadapted | xargs printf "%.*f\n" 2` # rounding to 2 digits
@@ -100,7 +118,7 @@ do
     #sccomb=`echo $sccomb | xargs printf "%.*f\n" 2` # rounding to 2 digits
 
 
-    ganho=`bc <<< "scale = 3; $bits / $i"`; # ganho da Efic. Espec. sobre OFDM
+    ganho=`bc <<< "scale = 3; $bits / $i"`; # ganho da Efic. Espec. sobre OFDM BPSK
     ganho=`echo $ganho | xargs printf "%.*f\n" 2` # rounding to 2 digits
     echo $i #$ganho
     echo $i $ganho >> se-gain.txt
@@ -113,7 +131,7 @@ sed -i 's/,/./g' se-gain.txt
 
 for j in {1..2..1} 
 do
-    rm -rf "IxS-$j"
+    sudo rm -rf "IxS-$j"
     mkdir "IxS-$j"
     mkdir "IxS-$j/amostras"
     mkdir "IxS-$j/resultados"
@@ -121,6 +139,6 @@ do
     sudo mv resultado-* "IxS-$j/resultados"
 done
 gnuplot plot-runtime &> /dev/null # suppresses impression
-#gnuplot plot-scthroughput &> /dev/null # suppresses impression
+gnuplot plot-sc &> /dev/null # suppresses impression
 evince runtime.eps &
-#evince throughput.eps &
+evince sc.eps &
